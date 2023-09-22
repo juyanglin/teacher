983b65efc6473e9cfffcbe9644f372e1ffab563c
juyanglin <49740728+juyanglin@users.noreply.github.com>
2023-09-22 09:31:45 +0800

    v2

---
+++ teacher.js
@@ -0,0 +1,124 @@
+function link( )
+		{
+            alert('許瑞陽')
+        } 
+		function link1( )
+		{
+            alert('郭盈傑')
+        } 
+		function link2( )
+		{
+            alert('林美珠')
+        } 
+		function link3( )
+		{
+            alert('顏慶瑩')
+        }
+		function link4( )
+		{
+            alert('曾鈺銘')
+        } 
+		
+        function link5( )
+		{
+            alert('溫善淳')
+        }
+			
+        function link71( )
+		{
+            alert('林建安')
+        } 
+		 function link72( )
+		{
+            alert('林如洋')
+        } 
+		 function link73( )
+		{
+            alert('沈孟儒')
+        } 
+		 function link74( )
+		{
+            alert('黃詩惠')
+        } 
+		 function link76( )
+		{
+            alert('葉怡秀')
+        } 
+		 function link75( )
+		{
+            alert('張英珍')
+        } 
+		 function link11( )
+		{
+            alert('林淑妙')
+        } 
+		 function link13( )
+		{
+            alert('陳靜卿')
+        } 
+         function link21( )
+		{
+            alert('張麗美')
+        } 
+		 function link23( )
+		{
+            alert('陳舜明')
+        } 
+		 function link31( )
+		{
+            alert('簡郁雅')
+        } 
+		 function link33( )
+		{
+            alert('郭雅萍')
+        } 
+		  function link41( )
+		{
+            alert('涂大節')
+        } 
+		 function link43( )
+		{
+            alert('吳美潔')
+        } 
+		 function link51( )
+		{
+            alert('蔡旻家')
+        } 
+		 function link52( )
+		{
+            alert('邱渝婷')
+        } 
+		
+		 function link61( )
+		{
+            alert('林士傑')
+        } 
+		 function link62( )
+		{
+            alert('葉建宏')
+        } 
+		 function link63( )
+		{
+            alert('')
+        } 
+		
+        function link34( )
+		{
+            alert('劉瑞德,闕妤甄')
+        } 
+		 function link100( )
+		{
+            alert('何月娥,李佩瑾,')
+        } 
+        function link82( )
+		{
+            alert('呂琬琪')
+        }
+        function link81( )
+		{
+            alert('陳冠伶')
+        }
+        function link80( )
+		{
+            alert('郭馨鎂')
+        }
